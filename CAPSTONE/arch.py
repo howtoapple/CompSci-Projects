3e46938d4f0f67adb64578c38061695ebd7e18c1
Jacob <jacobgon84@hotmail.com>
2022-04-14 11:30:51 -0600

    e

---
+++ CAPSTONE/arch.py
@@ -0,0 +1,58 @@
+#Arch?
+import os
+print("""
+
+
+ 
+                 .88888888:.
+                88888888.88888.
+              .8888888888888888.
+              888888888888888888
+              88' _`88'_  `88888
+              88 88 88 88  88888
+              88_88_::_88_:88888
+              88:::,::,:::::8888              Tux The Penguin
+              88`:::::::::'`8888              ------------------
+             .88  `::::'    8:88.             Linux
+            8888            `8:888.           For more info:
+          .8888'             `888888.         https://linux.org/
+         .8888:..  .::.  ...:'8888888:.
+        .8888.'     :'     `'::`88:88888
+       .8888        '         `.888:8888.
+      888:8         .           888:88888
+    .888:88        .:           888:88888:
+    8888888.       ::           88:888888
+    `.::.888.      ::          .88888888
+   .::::::.888.    ::         :::`8888'.:.
+  ::::::::::.888   '         .::::::::::::
+  ::::::::::::.8    '      .:8::::::::::::.
+ .::::::::::::::.        .:888:::::::::::::
+ :::::::::::::::88:.__..:88888:::::::::::'
+  `'.:::::::::::88888888888.88:::::::::'
+miK     `':::_:' -- '' -'-' `':_::::'`""")
+
+distro = input("Select your Distro!\n [A] Arch\n [U] Ubuntu\n [M] Manjaro\n [M] Mint\n");distro=distro.upper()
+
+if distro == "A":
+    os.system('clear')
+    print("""                    /-
+                   ooo:
+                  yoooo/
+                 yooooooo
+                yooooooooo
+               yooooooooooo
+             .yooooooooooooo                GenicStudios Presents...
+            .oooooooooooooooo
+           .oooooooarcoooooooo              ArchLinux
+          .ooooooooo-oooooooooo             ---------------------- 
+         .ooooooooo-  oooooooooo            The Video Game
+        :ooooooooo.    :ooooooooo           ----------------------
+       :ooooooooo.      :ooooooooo          Created with <3
+      :oooarcooo         .oooarcooo         Kernal: 5.16.5-arch
+     :ooooooooy           .ooooooooo        Terminal: Content Not Included
+    :ooooooooo   /ooooooooooooooooooo       
+   :ooooooooo      .-ooooooooooooooooo.   
+  ooooooooo-             -ooooooooooooo.  
+ ooooooooo-                 .-oooooooooo. 
+ooooooooo.                     -ooooooooo  
+""")
