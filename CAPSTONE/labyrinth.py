b13dfb06debfc09670333f365ab0f347c3e1e2ab
Jacob <jacobgon84@hotmail.com>
2022-04-15 22:29:11 -0600

    Labyrinth

--- CAPSTONE/labyrinth.py
+++ CAPSTONE/labyrinth.py
@@ -1,2 +1,19 @@
 #Code for the Game within the game
 #Labyrinth Game from Hawaii Part II
+print("""
+████████████████████████████
+█                          █
+█                          █
+█                          █
+█                          █
+█        Labyrinth         █
+█   ミラクルミュージカル   █
+█                          █ 
+█                          █
+█                          █
+█                          █
+█                          █
+█                          █
+████████████████████████████
+
+    """)
